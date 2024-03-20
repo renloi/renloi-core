872e64949689bf5b5ba01c7ec8823fc364b86b83
GiladLeef <96906027+GiladLeef@users.noreply.github.com>
2024-03-20 11:37:07 +0200

    ethereum testnet removed

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -25,12 +25,6 @@ var MainnetBootnodes = []string{
 	"enode://ca7a1f191c9d34117d2f03d8298e3465da68a91a6bef701a6e5aef6ea64290ffe80aa0f893ae3b1b908ef10db2fdd1eeafa8bb713da2ce21337319cc2b86b593@64.226.81.152:32668",
 }
 
-// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
-var TestnetBootnodes = []string{
-	"enode://924543a43d18bc5759a8bdcd17fa9c7c35df63968e9333640b80b58dab94b17a012371c9d46bed10ce7508a607cac76828ca04685893958eee44ade83b856dc2@47.242.237.63:32668",
-	"enode://ebad898d980b520ef6adb54ffb6a68117686e7332f1ea01f7551b7a296a34dd945445a078d7cad019d864c5ef0e0b7f2b5777d94f93adf7dc59f798af72609ac@47.242.235.121:32668",
-}
-
 var V5Bootnodes = []string{}
 
 // KnownDNSNetwork returns the address of a public DNS-based node list for the given
