af41793849b221465ec42e4479ab1d862ae0705b
Gilad Leef <96906027+GiladLeef@users.noreply.github.com>
2024-03-25 07:47:12 +0200

    Update startvali.sh

--- startvali.sh
+++ startvali.sh
@@ -1,5 +1,5 @@
 renloi \
-    --datadir ./chaindata/node2 \
+    --datadir ./chaindata/node1 \
     --networkid 268 \
     --mine \
     --port 32669 \
@@ -8,7 +8,7 @@ renloi \
     --gpo.maxprice 100 \
     --gpo.ignoreprice 0 \
     --unlock 0 \
-    --password ./chaindata/node2/pass.txt \
+    --password ./chaindata/node1/pass.txt \
     --syncmode=full \
     --snapshot=false \
     console
